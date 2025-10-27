824307cafbd76235a27de646df0d4d2ea0b560d0
Dlzar <dlzarcr55@gmail.com>
2025-10-27 22:19:03 +0300

    Sending Email
    seting up a mailtrap tool for testing email about resetting the password

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,11 +5,14 @@ use App\Models\Metric;
 use App\Models\RecentActivity;
 use App\Models\UserPreference;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Notification;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
+       use HasFactory, Notifiable;
     //
     protected $fillable=[
         'name',
